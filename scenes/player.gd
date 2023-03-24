43d14ed2ee993e41d63a129f107215375638a1f1
Elías Zelada <elixs.777@gmail.com>
2023-03-24 11:19:17 -0300

    animations player

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -100.0
-const ACCELERATION = 1000
+const ACCELERATION = 100
 const GRAVITY = 200
 
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
+@onready var pivot = $Pivot
 
 func _physics_process(delta):
 	if not is_on_floor():
+		velocity
 		velocity.y += GRAVITY * delta
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -19,4 +24,13 @@ func _physics_process(delta):
 	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION * delta)
 	
 	move_and_slide()
-
+	
+	# animation
+	
+	if abs(velocity.x) > 10 or move_input:
+		playback.travel("run")
+	else:
+		playback.travel("idle")
+	
+	if move_input:
+		pivot.scale.x = sign(move_input)
