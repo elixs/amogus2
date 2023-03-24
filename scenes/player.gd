54474df5d912564383385c3f1f26d03028cbabd4
Elías Zelada <elixs.777@gmail.com>
2023-03-24 11:40:44 -0300

    added jump animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,13 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -100.0
-const ACCELERATION = 100
-const GRAVITY = 200
+const ACCELERATION = 1000
+const GRAVITY = 150
 
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 @onready var pivot = $Pivot
+
+func _ready():
+	animation_tree.active = true
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -27,10 +30,16 @@ func _physics_process(delta):
 	
 	# animation
 	
-	if abs(velocity.x) > 10 or move_input:
-		playback.travel("run")
+	if is_on_floor():
+		if abs(velocity.x) > 10 or move_input:
+			playback.travel("run")
+		else:
+			playback.travel("idle")
 	else:
-		playback.travel("idle")
+		if velocity.y < 0:
+			playback.travel("jump")
+		else:
+			playback.travel("fall")
 	
 	if move_input:
 		pivot.scale.x = sign(move_input)
